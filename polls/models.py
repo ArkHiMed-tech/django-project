d1edaf0098923be8b0b90790aa8c45b90feecb95
ArkHiMed-tech <74063313+ArkHiMed-tech@users.noreply.github.com>
2022-11-29 20:39:04 +0200

    upadte

--- polls/models.py
+++ polls/models.py
@@ -4,6 +4,15 @@ from django.db import models
 class New(models.Model):
     name = models.CharField(max_length=30)
     main_text = models.CharField(max_length=300)
-    pub_date = models.DateTimeField('date published')
+    pub_date = models.DateTimeField('date published', auto_now=True)
     author = models.CharField(max_length=30)
     image = models.ImageField(upload_to='images')
+
+class Event(models.Model):
+    name = models.CharField(max_length=30)
+    description = models.CharField(max_length=200)
+    event_start_date = models.DateTimeField('event start date', null=True, blank=True)
+    event_end_date = models.DateTimeField('event end date', null=True, blank=True)
+    pub_date = models.DateTimeField('date published', auto_now=True)
+    author = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='images', null=True, blank=True)
